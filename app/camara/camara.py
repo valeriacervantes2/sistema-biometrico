a27c9786524cea6d6af3243c38f1d0271ba02e2f
Abigail M <abisu1206@gmail.com>
2026-03-03 13:28:21 -0600

    Deteccion de Landmarks

--- app/camara/camara.py
+++ app/camara/camara.py
@@ -1,42 +1,15 @@
 import cv2
 
-face_cascade = cv2.CascadeClassifier(
-    cv2.data.haarcascades + 'haarcascade_frontalface_default.xml'
-    )
+def iniciar_camara():
+    cap = cv2.VideoCapture(0)  # Inicia la cámara (0 es el índice de la cámara predeterminada)
+    if not cap.isOpened():
+        print("No se pudo abrir la cámara")
+        return None         
+    return cap
 
-#Esto es para inicializar la cámara
-cap = cv2.VideoCapture(0)
-
-if not cap.isOpened():
-    print("Cannot open camera")
-    exit()
-
-while True:
-    ret, frame = cap.read()
+def obtener_frame(cap):
+    ret, frame = cap.read()  # Lee un frame de la cámara
     if not ret:
-        print("Can't receive frame.")
-        break
-
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    gray = cv2.equalizeHist(gray)  # Mejora el contraste de la imagen
-
-    #Ajusta los parametros de detección
-    faces = face_cascade.detectMultiScale(
-        gray,
-        scaleFactor=1.3,    #Escala de reducción de la imagen para la detección, en este caso se reduce un 30% cada vez.
-        minNeighbors=5,     #Se usa para confirmar que es realemente una cara, en este caso se verifica 5 veces.
-        minSize=(30, 30)    #Tamaño mínimo de la cara a detectar, en este caso 30x30 píxeles
-    
-    )
-
-    #Dibuja el rectangulo detector de rostro
-    for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), ( 0, 255, 0), 2)
-
-    cv2.imshow('Face Detection', frame)
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-cap.release()
-cv2.destroyAllWindows()
+        print("No se pudo leer el frame")
+        return None
+    return frame
--- main.py
+++ main.py
@@ -1,4 +1,38 @@
+import cv2
+from app.camara.camara import iniciar_camara, obtener_frame
+from app.detection.detector_rostro import procesar_frame
 from app.database.database import inicializar_bd
 
-if __name__ == "__main__":
+
+def main():
+    #Inicializar la base de datos
     inicializar_bd()
+
+    #Iniciar la cámara
+    cap = iniciar_camara()
+    if cap is None:
+        return
+
+    while True:
+        frame = obtener_frame(cap)
+        if frame is None:
+            break
+
+        #Aquí se procesaría el frame con el detector de rostro
+        frame_procesado, face_encoding, mensaje = procesar_frame(frame)
+
+        print(mensaje)
+
+        #Mostrar el frame procesado
+        cv2.imshow('Cámara', frame_procesado)
+
+        #Salir con la tecla 'q'
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    
+    #Liberar la cámara y cerrar las ventanas
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
