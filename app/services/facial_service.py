d233378a26102c516e674887e4fc2903c71a5ba1
valeria <vale_cerv@outlook.com>
2026-03-04 09:46:26 -0600

    pruebas

--- app/services/facial_service.py
+++ app/services/facial_service.py
@@ -3,7 +3,7 @@ import numpy as np
 from datetime import datetime
 from database import get_connection
 
-
+#-------------
 # ==============================
 # CREAR USUARIO
 # ==============================
